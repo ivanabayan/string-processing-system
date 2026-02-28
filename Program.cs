39b043d5365a1967c66cc034ee137b34bb875c87
ivanabayan <sowzyivan@gmail.com>
2026-02-28 11:22:57 +0800

    Add Program.cs to initialize StringView

---
+++ Program.cs
@@ -0,0 +1,12 @@
+using StringProcessingApp.Views;
+
+namespace StringProcessingApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            new StringView().Start();
+        }
+    }
+}
